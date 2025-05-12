1c3d730255edd02ad35658d5223d2a865a60aa6c
Ankita Porwal <aporwal@venacorp.com>
2025-05-12 17:56:53 +0530

    Initial Commit

--- src/A4ThreadPoolmageProcessor/ImageProcessor.java
+++ src/A4ThreadPoolmageProcessor/ImageProcessor.java
@@ -43,3 +43,15 @@ public class ImageProcessor {
         System.out.println("Total execution time: " + (endTime - startTime) + " ms");
     }
 }
+
+//
+//Assignment 4: Thread Pool Image Processor
+//Background:
+//Simulate a parallel image processing pipeline using Java’s ExecutorService.
+//Requirements:
+//Given 50 image IDs (1–50), simulate processing (sleep 100ms)
+//Return a message "Image X processed" for each
+//Collect and print results in order using Callable + Future
+//Bonus Requirements:
+//Use a fixed thread pool of size 5
+//Measure total execution time
